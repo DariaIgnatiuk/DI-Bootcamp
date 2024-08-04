d3f001e7b02a3d8b2a0dde8213486d3b3520b6b2
Daria Ingatiuk <Ignatyuk.dariaa@gmail.com>
2024-08-04 14:32:17 +0300

    Initial commit

---
+++ Week1/Day1/ExerciseXP/Exercise1.py
@@ -0,0 +1,11 @@
+# Exercise 1 : Hello World
+# Instructions
+# Print the following output in one line of code:
+
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+
+for step in range (0, 4):
+    print("Hello world")
